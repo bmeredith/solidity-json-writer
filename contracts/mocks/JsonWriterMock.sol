f56abd3357d829c5431385f11ca654fe51503b7a
Ben Meredith <1832575+bmeredith@users.noreply.github.com>
2021-07-01 15:52:12 -0500

    -added tests of outputting properties for primitive types

--- contracts/mocks/JsonWriterMock.sol
+++ contracts/mocks/JsonWriterMock.sol
@@ -27,16 +27,32 @@ contract JsonWriterMock {
         return JsonWriter.writeAddressValue(json, value);
     }
 
+    function writeBooleanProperty(JsonWriter.Json memory json, string memory propertyName, bool value) public pure returns (JsonWriter.Json memory) {
+        return JsonWriter.writeBooleanProperty(json, propertyName, value);
+    }
+
     function writeBooleanValue(JsonWriter.Json memory json, bool value) public pure returns (JsonWriter.Json memory) {
         return JsonWriter.writeBooleanValue(json, value);
+    }
+
+    function writeIntProperty(JsonWriter.Json memory json, string memory propertyName, int value) public pure returns (JsonWriter.Json memory) {
+        return JsonWriter.writeIntProperty(json, propertyName, value);
     }
 
     function writeIntValue(JsonWriter.Json memory json, int value) public pure returns (JsonWriter.Json memory) {
         return JsonWriter.writeIntValue(json, value);
     }
 
+    function writeStringProperty(JsonWriter.Json memory json, string memory propertyName, string memory value) public pure returns (JsonWriter.Json memory) {
+        return JsonWriter.writeStringProperty(json, propertyName, value);
+    }
+
     function writeStringValue(JsonWriter.Json memory json, string memory value) public pure returns (JsonWriter.Json memory) {
         return JsonWriter.writeStringValue(json, value);
+    }
+
+    function writeUintProperty(JsonWriter.Json memory json, string memory propertyName, uint value) public pure returns (JsonWriter.Json memory) {
+        return JsonWriter.writeUintProperty(json, propertyName, value);
     }
 
     function writeUintValue(JsonWriter.Json memory json, uint value) public pure returns (JsonWriter.Json memory) {
--- test/JsonWriter.test.js
+++ test/JsonWriter.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 
-describe('JsonWriter', function() {
+describe('JsonWriter', function () {
 
   beforeEach(async function () {
     const JsonWriterMock = await ethers.getContractFactory('JsonWriterMock');
@@ -24,7 +24,7 @@ describe('JsonWriter', function() {
     });
   });
 
-  describe('objects', function() {
+  describe('objects', function () {
     it('creates empty object for an initial JSON string', async function () {
       let json = createJsonObject();
       json = await jsonWriter.writeStartObject(json);
@@ -34,31 +34,67 @@ describe('JsonWriter', function() {
     });
   });
 
-  describe('addresses', function() {
-    const tests = [
-      {arg: '0x0000000000000000000000000000000000000000', expected: '"0x0000000000000000000000000000000000000000"'},
-      {arg: '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', expected: '"0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"'}
-    ];
+  describe('addresses', function () {
+    describe('properties', function () {
+      const tests = [
+        { arg: '0x0000000000000000000000000000000000000000', expected: '{"prop": "0x0000000000000000000000000000000000000000"}' },
+        { arg: '0x1111111111111111111111111111111111111111', expected: '{"prop": "0x1111111111111111111111111111111111111111"}' },
+        { arg: '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', expected: '{"prop": "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"}' }
+      ];
 
-    tests.forEach(({arg, expected}) => {
-      it(`writes address value of ${arg}`, async function() {
-        let json = createJsonObject();
-        json = await jsonWriter.writeStringValue(json, arg);
-        
-        expect(json.value).to.equal(expected);
+      tests.forEach(({ arg, expected }) => {
+        it(`writes address property of ${arg}`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeStartObject(json);
+          json = await jsonWriter.writeStringProperty(json, 'prop', arg);
+          json = await jsonWriter.writeEndObject(json);
+
+          expect(json.value).to.equal(expected);
+        });
+      });
+    });
+
+    describe('values', function () {
+      const tests = [
+        { arg: '0x0000000000000000000000000000000000000000', expected: '"0x0000000000000000000000000000000000000000"' },
+        { arg: '0x1111111111111111111111111111111111111111', expected: '"0x1111111111111111111111111111111111111111"' },
+        { arg: '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF', expected: '"0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"' }
+      ];
+
+      tests.forEach(({ arg, expected }) => {
+        it(`writes address value of ${arg}`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeStringValue(json, arg);
+
+          expect(json.value).to.equal(expected);
+        });
       });
     });
   });
 
-  describe('booleans', function() {    
-    it('writes true boolean value', async function () {
+  describe('booleans', function () {
+    it('writes boolean property of \'true\'', async function () {
+      let json = createJsonObject();
+      json = await jsonWriter.writeBooleanProperty(json, 'prop', true);
+
+      expect(json.value).to.equal('"prop": true');
+    });
+
+    it('writes boolean property of \'false\'', async function () {
+      let json = createJsonObject();
+      json = await jsonWriter.writeBooleanProperty(json, 'prop', false);
+
+      expect(json.value).to.equal('"prop": false');
+    });
+
+    it('writes boolean value of \'true\'', async function () {
       let json = createJsonObject();
       json = await jsonWriter.writeBooleanValue(json, true);
 
       expect(json.value).to.equal('true');
     });
-    
-    it('writes false boolean value', async function () {
+
+    it('writes boolean value of \'false\'', async function () {
       let json = createJsonObject();
       json = await jsonWriter.writeBooleanValue(json, false);
 
@@ -66,64 +102,136 @@ describe('JsonWriter', function() {
     });
   });
 
-  describe('ints', function() {
-    const tests = [
-      {arg: -1, expected: '-1'},
-      {arg: 0, expected: '0'},
-      {arg: 1, expected: '1'},
-      {
-        arg: '-57896044618658097711785492504343953926634992332820282019728792003956564819968', 
-        expected: '-57896044618658097711785492504343953926634992332820282019728792003956564819968'
-      },
-      {
-        arg: '57896044618658097711785492504343953926634992332820282019728792003956564819967', 
-        expected: '57896044618658097711785492504343953926634992332820282019728792003956564819967'
-      }
-    ];
+  describe('ints', function () {
+    describe('properties', function () {
+      const tests = [
+        { arg: -1, expected: '"prop": -1' },
+        { arg: 0, expected: '"prop": 0' },
+        { arg: 1, expected: '"prop": 1' },
+        {
+          arg: '-57896044618658097711785492504343953926634992332820282019728792003956564819968',
+          expected: '"prop": -57896044618658097711785492504343953926634992332820282019728792003956564819968'
+        },
+        {
+          arg: '57896044618658097711785492504343953926634992332820282019728792003956564819967',
+          expected: '"prop": 57896044618658097711785492504343953926634992332820282019728792003956564819967'
+        }
+      ];
 
-    tests.forEach(({arg, expected}) => {
-      it(`writes int value of ${arg}`, async function() {
-        let json = createJsonObject();
-        json = await jsonWriter.writeIntValue(json, arg);
+      tests.forEach(({ arg, expected }) => {
+        it(`writes int property of ${arg}`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeIntProperty(json, 'prop', arg);
 
-        expect(json.value).to.equal(expected);
+          expect(json.value).to.equal(expected);
+        });
+      });
+    });
+
+    describe('values', function () {
+      const tests = [
+        { arg: -1, expected: '-1' },
+        { arg: 0, expected: '0' },
+        { arg: 1, expected: '1' },
+        {
+          arg: '-57896044618658097711785492504343953926634992332820282019728792003956564819968',
+          expected: '-57896044618658097711785492504343953926634992332820282019728792003956564819968'
+        },
+        {
+          arg: '57896044618658097711785492504343953926634992332820282019728792003956564819967',
+          expected: '57896044618658097711785492504343953926634992332820282019728792003956564819967'
+        }
+      ];
+
+      tests.forEach(({ arg, expected }) => {
+        it(`writes int value of ${arg}`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeIntValue(json, arg);
+
+          expect(json.value).to.equal(expected);
+        });
       });
     });
   });
 
-  describe('strings', function() {
-    const tests = [
-      {arg: '', expected: '""'},
-      {arg: 'test', expected: '"test"'},
-      {arg: '1234', expected: '"1234"'}
-    ];
+  describe('strings', function () {
+    describe('properties', function () {
+      const tests = [
+        { arg: '', expected: '"prop": ""' },
+        { arg: 'test', expected: '"prop": "test"' },
+        { arg: '1234', expected: '"prop": "1234"' },
+        { arg: '!@#$%^&*();<>?,.:\'"{}[]', expected: '"prop": "!@#$%^&*();<>?,.:\'"{}[]"' },
+        { arg: '💯', expected: '"prop": "💯"' }
+      ];
 
-    tests.forEach(({arg, expected}) => {
-      it(`writes string value of '${arg}'`, async function() {
-        let json = createJsonObject();
-        json = await jsonWriter.writeStringValue(json, arg);
-        
-        expect(json.value).to.equal(expected);
+      tests.forEach(({ arg, expected }) => {
+        it(`writes string property of '${arg}'`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeStringProperty(json, 'prop', arg);
+
+          expect(json.value).to.equal(expected);
+        });
+      });
+    });
+
+    describe('values', function () {
+      const tests = [
+        { arg: '', expected: '""' },
+        { arg: 'test', expected: '"test"' },
+        { arg: '1234', expected: '"1234"' },
+        { arg: '!@#$%^&*();<>?,.:\'"{}[]', expected: '"!@#$%^&*();<>?,.:\'"{}[]"' },
+        { arg: '💯', expected: '"💯"' }
+      ];
+
+      tests.forEach(({ arg, expected }) => {
+        it(`writes string value of '${arg}'`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeStringValue(json, arg);
+
+          expect(json.value).to.equal(expected);
+        });
       });
     });
   });
 
-  describe('uints', function() {
-    const tests = [
-      {arg: 0, expected: '0'},
-      {arg: 1, expected: '1'},
-      {
-        arg: '115792089237316195423570985008687907853269984665640564039457584007913129639935', 
-        expected: '115792089237316195423570985008687907853269984665640564039457584007913129639935'
-      }
-    ];
+  describe('uints', function () {
+    describe('properties', function () {
+      const tests = [
+        { arg: 0, expected: '"prop": 0' },
+        { arg: 1, expected: '"prop": 1' },
+        {
+          arg: '115792089237316195423570985008687907853269984665640564039457584007913129639935',
+          expected: '"prop": 115792089237316195423570985008687907853269984665640564039457584007913129639935'
+        }
+      ];
+  
+      tests.forEach(({ arg, expected }) => {
+        it(`writes uint property of ${arg}`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeUintProperty(json, 'prop', arg);
+  
+          expect(json.value).to.equal(expected);
+        });
+      });
+    });
 
-    tests.forEach(({arg, expected}) => {
-      it(`writes uint value of ${arg}`, async function() {
-        let json = createJsonObject();
-        json = await jsonWriter.writeUintValue(json, arg);
-
-        expect(json.value).to.equal(expected);
+    describe('values', function () {
+      const tests = [
+        { arg: 0, expected: '0' },
+        { arg: 1, expected: '1' },
+        {
+          arg: '115792089237316195423570985008687907853269984665640564039457584007913129639935',
+          expected: '115792089237316195423570985008687907853269984665640564039457584007913129639935'
+        }
+      ];
+  
+      tests.forEach(({ arg, expected }) => {
+        it(`writes uint value of ${arg}`, async function () {
+          let json = createJsonObject();
+          json = await jsonWriter.writeUintValue(json, arg);
+  
+          expect(json.value).to.equal(expected);
+        });
       });
     });
   });
