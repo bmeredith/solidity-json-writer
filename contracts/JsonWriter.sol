92dabae08798a48d4556dc2c1a9d9b439f677d31
Ben Meredith <1832575+bmeredith@users.noreply.github.com>
2021-07-02 18:14:45 -0500

    removed unused methods

--- contracts/JsonWriter.sol
+++ contracts/JsonWriter.sol
@@ -418,24 +418,6 @@ library JsonWriter {
         return json;
     }
 
-    function getBoolean(uint256 _packedBools, uint256 _boolNumber)
-        public
-        pure
-        returns (bool)
-    {
-        uint256 flag = (_packedBools >> _boolNumber) & uint256(1);
-        return (flag == 1 ? true : false);
-    }
-
-    function setBoolean(
-        uint256 _packedBools,
-        uint256 _boolNumber,
-        bool _value
-    ) public pure returns (uint256) {
-        if (_value) return _packedBools | (uint256(1) << _boolNumber);
-        else return _packedBools & ~(uint256(1) << _boolNumber);
-    }
-
     /**
      * @dev Escapes any characters that required by JSON to be escaped.
      */
