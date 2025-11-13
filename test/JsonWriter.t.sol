92fd8f000884f49a186860779322b7578853061b
bmeredith <1832575+bmeredith@users.noreply.github.com>
2025-11-12 23:58:53 -0600

    switched over to foundry

---
+++ test/JsonWriter.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Test} from "forge-std/Test.sol";
+import {JsonWriter} from "../src/JsonWriter.sol";
+
+contract JsonWriterTest is Test {
+}
